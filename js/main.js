8741234a0e963da17a3ee5d31414a50fb4007bb4
adri <adri.kolaj@gmail.com>
2020-12-20 15:55:00 +0100

    ricerca

--- js/main.js
+++ js/main.js
@@ -2,23 +2,35 @@ var app = new Vue ({
 
 
   el: '#root',
+
   data: {
+    searchNew: '',
     searchedFilm: [],
   },
-  mounted: function() {
-    axios.get('https://api.themoviedb.org/3/search/movie', {
-      params: {
-        api_key: '93b4dae4e614249a3c440ffbcd0c00e1',
-        language: 'it-IT',
-        query: 'ritorno-al-futuro'
+
+  // methods: {
+  //   goOnSearch: function() {
+  //     return this.searchNew
+  //   }
+  // },
+
+  methods: {
+    goOnSearch() {
+
+
+
+      axios.get('https://api.themoviedb.org/3/search/movie', {
+        params: {
+          api_key: '93b4dae4e614249a3c440ffbcd0c00e1',
+          language: 'it-IT',
+          query: this.searchNew,
+        }
+      })
+        .then((response) => {
+          this.searchedFilm = response.data.results;
+        });
       }
-    })
-      .then((response) => {
-        // console.log(response.data.adult);
-        this.searchedFilm = response.data.results;
-        console.log(this.searchedFilm)
-      });
-  }
+    },
 
 
 })
