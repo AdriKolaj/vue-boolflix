4404847fc0a4b46b79f6ecadff716416431f7869
adri <adri.kolaj@gmail.com>
2020-12-21 13:26:49 +0100

    milestone 2

--- js/main.js
+++ js/main.js
@@ -6,31 +6,41 @@ var app = new Vue ({
   data: {
     searchNew: '',
     searchedFilm: [],
+    flags: ['it', 'en', 'de']
   },
-
-  // methods: {
-  //   goOnSearch: function() {
-  //     return this.searchNew
-  //   }
-  // },
 
   methods: {
     goOnSearch() {
 
+      if(this.searchNew != '') {
 
+        axios.get('https://api.themoviedb.org/3/search/movie', {
+          params: {
+            api_key: '93b4dae4e614249a3c440ffbcd0c00e1',
+            language: 'it-IT',
+            query: this.searchNew,
+          }
+        })
+        .then((response) => {
+          this.searchedFilm = response.data.results;
+        });
 
-      axios.get('https://api.themoviedb.org/3/search/movie', {
-        params: {
-          api_key: '93b4dae4e614249a3c440ffbcd0c00e1',
-          language: 'it-IT',
-          query: this.searchNew,
-        }
-      })
+        axios.get('https://api.themoviedb.org/3/search/tv', {
+          params: {
+            api_key: '93b4dae4e614249a3c440ffbcd0c00e1',
+            language: 'it-IT',
+            query: this.searchNew,
+          }
+        })
         .then((response) => {
           this.searchedFilm = response.data.results;
         });
       }
     },
+    getStars(vote) {
+      return Math.ceil(vote / 2);
+    }
+  },
 
 
 })
